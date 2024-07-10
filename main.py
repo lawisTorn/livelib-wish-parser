7f8e910c526d33d2e638ce13493a5e26df90a427
tuhum <goga.kobyakov@gmail.com>
2024-07-10 21:02:01 +0500

    request

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 
-pages = int(input("all pages: "))
-name = input("name on livelib: ")
+pages = int(input("All pages: "))
+name = input("Name on LiveLib: ")
 
-for x in range(1,pages):
-    data = requests.get('https://www.livelib.ru/reader/{name}/wish/~{x}')
+for x in range(1, pages + 1):
+    url = f'https://www.livelib.ru/reader/{name}/wish/~{x}'
+    response = requests.get(url)
 
+    if response.status_code == 200:
+        data = response.text
+        soup = BeautifulSoup(data, 'lxml')
+        print(soup.prettify())
+    else:
+        print(f"Failed to retrieve page {x}")
